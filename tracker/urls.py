c0e568a406608c355f1c055e734229a4ff84452f
shreyasicon <ghsaishreyas3@gmail.com>
2025-11-17 23:04:21 +0000

    Changes in urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('edit/<int:planting_id>/', views.edit_planting_view, name='edit_planting'),
     path('update/<int:planting_id>/', views.update_planting, name='update_planting'),
     path('', views.index, name='index'),
-]
+] 
+
