24ee0a5880e1a42bff7e77a3b5083911f8ae973e
shreyasicon <ghsaishreyas3@gmail.com>
2025-11-18 18:19:50 +0000

    Changes login pages

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from core.views import health
 
 urlpatterns = [
     path('', include('tracker.urls')),      # site root -> tracker app
+    path('accounts/', include('django.contrib.auth.urls')),  # <-- add this line
     path('admin/', admin.site.urls),
     path('health', health, name='health'),
 ]
