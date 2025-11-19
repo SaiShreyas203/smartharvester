d08b8ce94da997b91199f83107c545d4ca9f110b
shreyasicon <ghsaishreyas3@gmail.com>
2025-11-19 00:12:42 +0000

    Changes in signup

--- tracker/views.py
+++ tracker/views.py
@@ -16,6 +16,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 DATA_FILE_PATH = os.path.join(settings.BASE_DIR, 'tracker', 'data.json')
 
