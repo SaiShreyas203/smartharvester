89f64db23fa5ad532bee927c039b6dd8c507abbf
shreyasicon <ghsaishreyas3@gmail.com>
2025-11-22 18:01:29 +0000

    User setup

--- tracker/signals.py
+++ tracker/signals.py
@@ -3,20 +3,19 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
-from . import dynamo
-
 logger = logging.getLogger(__name__)
-
 User = get_user_model()
-
 
 @receiver(post_save, sender=User)
 def sync_user_to_dynamo(sender, instance, created, **kwargs):
     """
     After a User is created/updated, write a corresponding item to the DynamoDB users table.
-    Non-blocking: log errors but do not raise so signup is not interrupted.
+    Lazy-import dynamo inside the handler to avoid circular import problems during app startup.
     """
     try:
+        # lazy import to avoid circular import errors during app registry ready()
+        from . import dynamo  # local module: tracker/dynamo.py
+
         payload = {
             "email": instance.email,
             "first_name": instance.first_name,
@@ -29,12 +28,14 @@ def sync_user_to_dynamo(sender, instance, created, **kwargs):
         else:
             logger.error("Dynamo sync returned False for user %s", instance.pk)
     except Exception as exc:
+        # Log exception but do not raise (do not break user signup)
         logger.exception("Exception syncing user %s to Dynamo: %s", instance.pk, exc)
 
 
 @receiver(post_delete, sender=User)
 def delete_user_from_dynamo(sender, instance, **kwargs):
     try:
+        from . import dynamo
         ok = dynamo.delete_user(user_id=instance.pk)
         if ok:
             logger.info("Dynamo delete succeeded for user %s", instance.pk)
